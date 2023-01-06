3982e201e3abc9615416d66107c91eaffd12a791
sasamue <murillo.samuel.2735@eam.edu.co>
2023-01-06 11:20:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# proyectosJavaScript
+# proyectosJavaScript
---
+++ calculadora1.js
@@ -0,0 +1,38 @@
+// suma , resta , multiplicacion , division
+a= 10;
+b= 7;
+ function suma(a,b){
+    resultado = a+b;
+    console.log(resultado)
+ }
+
+ suma(a,b);
+ console.log("El resultado de la operacion suma es", resultado);
+
+ c= 50;
+ d=24;
+ function resta (c,d){  
+    resultado= c-d;
+    console.log(resultado);
+ }
+
+ resta(c,d);
+ console.log("El resultado de la operacion resta es", resultado);
+
+ e=40;
+ f=68;
+function multiplicacion(e,f){
+    resultado= e*f;
+    console.log(resultado);
+}
+multiplicacion(e,f)
+console.log("El resultado de la operacion multiplicacion es", resultado);
+
+h=1456
+s=56
+function division(h,s){
+    resultado= h/s;
+    console.log(resultado);
+}
+division(h,s);
+console.log("El resultado de la operacion division es", resultado);
